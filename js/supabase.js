457302c1b57edb6905bb5f68fa962af7a67617e6
foconamissao <foconamissao.con@gmail.com>
2026-09-16 07:06:38 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = 'COLE_AQUI_SUA_SUPABASE_URL';
-const SUPABASE_ANON_KEY = 'COLE_AQUI_SUA_SUPABASE_ANON_KEY';
+const SUPABASE_URL = 'https://zjnbmgkvnzdnaiszqhta.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_vghGz23YWhZ4CNXMjkrslA_ZH1osJsI';
 
 if (!window.supabase) console.error('Supabase JS não carregou.');
 export const sb = (SUPABASE_URL.startsWith('http') && !SUPABASE_ANON_KEY.startsWith('COLE_'))
